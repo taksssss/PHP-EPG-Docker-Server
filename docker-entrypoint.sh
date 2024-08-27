002ba5600de5466aa763fa015279ceb6b1b8ea0c
Tak <decat2008@gmail.com>
2024-08-27 15:27:10 +0800

    修复空目录不存在导致的异常

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -42,17 +42,16 @@ sed -i 's/#LoadModule\ expires_module/LoadModule\ expires_module/' /etc/apache2/
 sed -i "s/memory_limit = .*/memory_limit = ${PHP_MEMORY_LIMIT}/" /etc/php83/php.ini
 sed -i "s#^;date.timezone =\$#date.timezone = \"${TZ}\"#" /etc/php83/php.ini
 
-
-# Change ownership of /htdocs/epg
-chown -R apache:apache /htdocs/epg
-
 # Check if /htdocs/epg/data/config.json exists
 echo 'Checking if /htdocs/epg/data/config.json exists'
 if [ ! -f /htdocs/epg/data/config.json ]; then
     echo 'config.json not found, copying default config'
     # If it does not exist, use config_default.json
-    cp /htdocs/epg/config_default.json /htdocs/epg/data/config.json
+    mkdir -p /htdocs/epg/data && cp -p /htdocs/epg/config_default.json /htdocs/epg/data/config.json
 fi
+
+# Change ownership of /htdocs/epg
+chown -R apache:apache /htdocs/epg
 
 echo 'Running cron.php and Apache'
 
